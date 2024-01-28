af3bc2f5d477293452d9aa2ac68a1341d5ad6845
dstartsev <mityastar01@gmail.com>
2024-01-28 06:03:58 +0500

    security and minors

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -25,5 +30,32 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route(path: '/register', name: 'app_register')]
+    public function register(Request $request, UserPasswordHasherInterface $passwordEncoder, ManagerRegistry $em): Response
+    {
+        if ($this->getUser() instanceof UserInterface) {
+            return $this->redirectToRoute('index');
+        }
+
+        if ($request->isMethod('POST')) {
+            $username = $request->request->get('username');
+            $password = $request->request->get('password');
+
+            $user = new User();
+            $user->setUsername($username);
+
+            $em = $em->getManager();
+            $encodedPassword = $passwordEncoder->hashPassword($user, $password);
+            $user->setPassword($encodedPassword);
+
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render('security/register.html.twig');
     }
 }
